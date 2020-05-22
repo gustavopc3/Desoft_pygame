96b4eb648d8d489c49fe54ad46920f7886611918
Gustavo Pinheiro <gustavopc3@al.insper.edu.br>
2020-05-22 19:06:38 -0300

    Início

--- jogo_pygame.py
+++ jogo_pygame.py
@@ -4,3 +4,26 @@ Authors: Gustavo Pinheiro
          Maria Eduarda Mourão
          Rafaela Zyingier
 """
+import pygame
+import sys
+"""Rotina principal do jogo"""
+def main():
+    #Inicializa as rotinas do jogo
+    pygame.init()
+    
+    #Muda o nome no título do pygame
+    pygame.display.set_caption("Octopy")
+
+    #Cria superfície para o jogo
+    superficie = pygame.display.set_mode([600, 600]) #Define o tamanho
+
+    arquivo = 
+
+    #Para colocar nossa imagem:
+    try:
+        img = pygame.image.load(" ").convert_alpha() #Convert_alpha: Desfaz o corte gerado de imagem ao converter a imagem para o padrão de tela mantendo as áreas diferentes.
+    except pygame.error:
+        print("Erro ao tentar ler imagem")
+        sys.exit()
+
+    #Cria nossa superfície de fundo:
